9a66ec1ae530b47648ee48b2575e6cf09667af79
humayun-dev <humayun.devv@gmail.com>
2024-07-05 14:22:42 +0500

    Dynamic arrays

--- dynamicArray.py
+++ dynamicArray.py
@@ -1,55 +1,79 @@
 ''' 
     1. Creating Dynamic Array
-    2. 
+    2. Length of the list
+    3. Append the list items
+    4. Print the list
+    5. Accessing elements via index
 '''
 import ctypes
 
-# 1.Creating Dynamic Array
+# 1. Creating Dynamic Array
 class DynamicArray:
     def __init__(self):
         self.size = 1
         self.n = 0          # input = 0
         # create a C type array with size = self.size
-        self.A = self.makeArray(self.size)
+        self.A = self.make_array(self.size)
     
-    def makeArray(self,capacity):
+    def make_array(self, capacity):
         # create a C type array(static, referential) with size capacity
         return (capacity * ctypes.py_object)()
     
-    # 2.length of the list
+    # 2. Length of the list
     def __len__(self):
         return self.n
     
-    # 3.append the list items
-    def __append__(self,item):
+    # 3. Append the list items
+    def append(self, item):
         if self.n == self.size:
             # if there isn't any space then resize
-            self.__resize(self.size * 2)
+            self.resize(self.size * 2)
         # if space exists then append
         self.A[self.n] = item
-        self.n = self.n + 1 
+        self.n += 1
     
-    # resize function
-    def __resize(self,new_capacity):
+    # Resize function
+    def resize(self, new_capacity):
         # create new array with new capacity
-        B = self.makeArray(new_capacity)
-        self.size = new_capacity
+        B = self.make_array(new_capacity)
         # copy the content of A to B
         for i in range(self.n):
             B[i] = self.A[i]
         # reassign A
         self.A = B
+        self.size = new_capacity
+    
+    # 4. Print the list
+    def __str__(self):
+        result = ', '.join(str(self.A[i]) for i in range(self.n))
+        return '[' + result + ']'
 
-# 1.create object of the above list which is part of the class DynamicArray
+    # 5. Accessing elements using index
+    def __getitem__(self, index):
+        if 0 <= index < self.n:
+            return self.A[index]
+        else:
+            raise IndexError('Index error: out of bound')
+
+# 1. Create object of the above list which is part of the class DynamicArray
 obj = DynamicArray()
 print(type(obj))   # to check the data type of the list
-print("\n")
-print(obj)  # it will print the address of the object
+print()
 
-# 2.length of the list
-print(len(obj))
+# 2. Length of the list
+print(len(obj))  # Output: 0
 
-# 3.append the items
-obj.__append__("hello")
-obj.__append__(3)
-print(len(obj))
+# 3. Append the items
+obj.append("hello")
+obj.append(3)
+obj.append(4.4)
+print(len(obj))  # Output: 3
+
+# 4. Print the list
+print(obj)  # Output: [hello, 3, 4.4]
+
+# 5. Accessing elements via index
+print(obj[2])  # Output: 4.4
+
+# Uncommenting the following line will raise IndexError
+# print(obj[5])  # IndexError: Index error: out of bound
